b479f73043f3de698ad62e11f808aeaf14f6fb36
Haley Akin <wasatchtimpanogos@outlook.com>
2023-01-04 11:08:57 -0700

    register save

---
+++ src/components/Register.jsx
@@ -0,0 +1,88 @@
+import React, { useRef, useState, useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from  "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () => {
+
+    const userRef = useRef()
+    const errRef = useRef()
+
+    const [user, setUser] = useState('')
+    const [validName, setValidName] = useState(false)
+    const [userFocus, setUserFocus] = useState(false)
+   
+
+    const [pwd, setPwd] = useState ('')
+    const [validPwd, setValidPwd] = useState(false)
+    const [pwdFocus, setPwdFocus] = useState(false)
+
+    const [matchPwd, setMatchPwd] = useState('')
+    const [validMatch, setValidMatch] = useState(false)
+    const [matchFocus, setMatchFocus] = useState(false)
+
+    const [errMsg, setErrMsg] = useState('')
+    const [success, setSuccess] = useState(false)
+
+    useEffect(() => {
+        userRef.current.focus()
+    }, [])
+
+    //useEffect for USER
+    useEffect(() => {
+        const result = USER_REGEX.test(user)
+        console.log(result)
+        console.log(user)
+        setValidName(result)
+    }, [user])
+
+    //useEffect for PASSWORD
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd)
+        console.log(result)
+        console.log(pwd)
+        setValidPwd(result)
+        const match = pwd === matchPwd
+        setValidMatch(match)
+    }, [pwd, matchPwd])
+
+    //useEffect for ErrMsg
+    useEffect(()=>{
+        setErrMsg('')
+    }, [user, pwd, matchPwd])
+
+
+    return (
+        <div>
+           <p ref={errRef} className={errMsg ? "errmsg" :
+           "offscreen"} aria-live="assertive">{errMsg}</p>
+
+           <h1>Register</h1>
+
+           <form>
+            <label htmlFor="username">
+                Username:
+            </label>
+            <input
+                type="text"
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                required
+                aria-invaild={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
+                ></input>
+                
+           </form>
+        </div>
+    )
+}
+
+export default Register
+
+
