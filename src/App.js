caf704bc01e6d7d1b25884766a2570765586d8e4
Haley Akin <wasatchtimpanogos@outlook.com>
2023-01-05 09:24:15 -0700

    save

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Register from './components/Register';
-import Login from './components/Login';
+
 
 function App() {
   return (
     <main className='App'>
       
-      <Login></Login>
-      {/* <Register></Register> */}
+      
+      <Register></Register>
       
     </main>
   );
